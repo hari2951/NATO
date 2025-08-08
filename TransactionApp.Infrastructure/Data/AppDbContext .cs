43a13416d7ae6ac4d72e0c000978af284649af03
Hari <harikrishnan.korasseril@monitoris.co.uk>
2025-08-08 22:43:53 +0100

    Adding the seed data

--- TransactionApp.Infrastructure/Data/AppDbContext .cs
+++ TransactionApp.Infrastructure/Data/AppDbContext .cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Globalization;
 using TransactionApp.Domain.Entities;
+using TransactionApp.Domain.Enums;
 
 namespace TransactionApp.Infrastructure.Data
 {
@@ -11,6 +12,7 @@ namespace TransactionApp.Infrastructure.Data
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
             CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
         }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
 
@@ -20,7 +22,114 @@ namespace TransactionApp.Infrastructure.Data
                 .Property(t => t.Amount)
                 .HasColumnType("decimal(18,2)");
 
+            var users = new List<User>
+            {
+                new() {
+                    Id = "10b42cd3-4690-4281-bc71-14679b0c7204",
+                    FirstName = "Alice",
+                    LastName = "Smith",
+                    Email = "alice@test.com"
+                },
+                new() {
+                    Id = "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7",
+                    FirstName = "Bob",
+                    LastName = "Johnson",
+                    Email = "bob@test.com"
+                },
+                new() {
+                     Id = "3178e886-113f-44b8-9c63-2a173ddfe45c",
+                     FirstName = "James",
+                     LastName = "West",
+                     Email = "James@test.com"
+                 },
+            };
+
+            modelBuilder.Entity<User>().HasData(users);
+
+            var transactions = new List<Transaction>
+            {
+                new() {
+                    Id = 1,
+                    UserId = "10b42cd3-4690-4281-bc71-14679b0c7204",
+                    Amount = 100.00m,
+                    TransactionType = TransactionTypeEnum.Debit,
+                    CreatedAt = new DateTime(2025, 1, 10)
+                },
+                new() {
+                    Id = 2,
+                    UserId = "10b42cd3-4690-4281-bc71-14679b0c7204",
+                    Amount = 250.00m,
+                    TransactionType = TransactionTypeEnum.Credit,
+                    CreatedAt = new DateTime(2025, 2, 15)
+                },
+                new() {
+                    Id = 3,
+                    UserId = "10b42cd3-4690-4281-bc71-14679b0c7204",
+                    Amount = 15.00m,
+                    TransactionType = TransactionTypeEnum.Debit,
+                    CreatedAt = new DateTime(2025, 7, 15)
+                },
+                new() {
+                    Id = 4,
+                    UserId = "10b42cd3-4690-4281-bc71-14679b0c7204",
+                    Amount = 5.00m,
+                    TransactionType = TransactionTypeEnum.Debit,
+                    CreatedAt = new DateTime(2025, 6, 01)
+                },
+                new() {
+                    Id = 5,
+                    UserId = "3178e886-113f-44b8-9c63-2a173ddfe45c",
+                    Amount = 50.00m,
+                    TransactionType = TransactionTypeEnum.Credit,
+                    CreatedAt = new DateTime(2025, 8, 01)
+                },
+                new() {
+                    Id = 6,
+                    UserId = "3178e886-113f-44b8-9c63-2a173ddfe45c",
+                    Amount = 75.00m,
+                    TransactionType = TransactionTypeEnum.Credit,
+                    CreatedAt = new DateTime(2025, 4, 20)
+                },
+                new() {
+                    Id = 7,
+                    UserId = "3178e886-113f-44b8-9c63-2a173ddfe45c",
+                    Amount = 16.00m,
+                    TransactionType = TransactionTypeEnum.Credit,
+                    CreatedAt = new DateTime(2025, 3, 13)
+                },
+                new() {
+                    Id = 8,
+                    UserId = "3178e886-113f-44b8-9c63-2a173ddfe45c",
+                    Amount = 22.00m,
+                    TransactionType = TransactionTypeEnum.Debit,
+                    CreatedAt = new DateTime(2025, 6, 04)
+                },
+                new() {
+                    Id = 9,
+                    UserId = "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7",
+                    Amount = 19.00m,
+                    TransactionType = TransactionTypeEnum.Credit,
+                    CreatedAt = new DateTime(2025, 5, 13)
+                },
+                new() {
+                    Id = 10,
+                    UserId = "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7",
+                    Amount = 79.00m,
+                    TransactionType = TransactionTypeEnum.Debit,
+                    CreatedAt = new DateTime(2025, 5, 29)
+                }
+            };
+
+            modelBuilder.Entity<Transaction>().HasData(transactions);
+
             base.OnModelCreating(modelBuilder);
+        }
+
+        private static DateTime RandomDateInCurrentYear(Random random)
+        {
+            var start = new DateTime(DateTime.Now.Year, 1, 1);
+            var range = (DateTime.Today - start).Days;
+            return start.AddDays(random.Next(range));
         }
     }
 }
---
+++ TransactionApp.Infrastructure/Migrations/20250808213841_SeedingUserAndTransactionData.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TransactionApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingUserAndTransactionData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { "10b42cd3-4690-4281-bc71-14679b0c7204", "alice@test.com", "Alice", "Smith" },
+                    { "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7", "bob@test.com", "Bob", "Johnson" },
+                    { "3178e886-113f-44b8-9c63-2a173ddfe45c", "James@test.com", "James", "West" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transactions",
+                columns: new[] { "Id", "Amount", "CreatedAt", "TransactionType", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 100.00m, new DateTime(2025, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "10b42cd3-4690-4281-bc71-14679b0c7204" },
+                    { 2, 250.00m, new DateTime(2025, 2, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "10b42cd3-4690-4281-bc71-14679b0c7204" },
+                    { 3, 15.00m, new DateTime(2025, 7, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "10b42cd3-4690-4281-bc71-14679b0c7204" },
+                    { 4, 5.00m, new DateTime(2025, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "10b42cd3-4690-4281-bc71-14679b0c7204" },
+                    { 5, 50.00m, new DateTime(2025, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "3178e886-113f-44b8-9c63-2a173ddfe45c" },
+                    { 6, 75.00m, new DateTime(2025, 4, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "3178e886-113f-44b8-9c63-2a173ddfe45c" },
+                    { 7, 16.00m, new DateTime(2025, 3, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "3178e886-113f-44b8-9c63-2a173ddfe45c" },
+                    { 8, 22.00m, new DateTime(2025, 6, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "3178e886-113f-44b8-9c63-2a173ddfe45c" },
+                    { 9, 19.00m, new DateTime(2025, 5, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7" },
+                    { 10, 79.00m, new DateTime(2025, 5, 29, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Transactions",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "10b42cd3-4690-4281-bc71-14679b0c7204");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "15cc7ba5-35c7-42dd-b688-d2bfa8c0f3a7");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "3178e886-113f-44b8-9c63-2a173ddfe45c");
+        }
+    }
+}
